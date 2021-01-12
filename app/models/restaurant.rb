f0535cff1daa8402a8dda9e506f97cb84b396f59
Hongjoo Yang <attinore@gmail.com>
2021-01-12 21:43:05 +0900

    created upto controllers

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+# restaurant model
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  validates :category, presence: true, inclusion: { in: CATEGORY }
+end
